f6eb3269c6eeda6555c9d79ecd49448f4df66faf
yeahbutstill <mayadani28@gmail.com>
2024-09-14 21:04:28 +0700

    Array Desctructuring

--- JavaScript/MenstrukturkanDatadenganObject/ArrayDestructuring.js
+++ JavaScript/MenstrukturkanDatadenganObject/ArrayDestructuring.js
@@ -2,3 +2,19 @@
 // dilakukan yaitu dengan menggunakan kurung siku yang di dalamnya adalah nama variabel
 // yang kita assign. Kemudian diikuti dengan assignment operator sama dengan (=) dan
 // Array yang ingin diambil nilainya.
+const introduction = ["Hello", "DNL", null, undefined, ];
+const [greeting, name, errorEnggaYa, masaSihNe, kosong] = introduction;
+console.log(greeting);
+console.log(name);
+console.log(errorEnggaYa);
+console.log(masaSihNe);
+console.log(kosong);
+
+// Dapat dilihat bahwa variabel greeting didapatkan dari Array introduction tanpa 
+// harus mengaksesnya menggunakan index. Destructuring Array mempermudah kita
+// untuk memecahkan struktur data menjadi bagian-bagian yang lebih kecil seperti 
+// contoh kode di atas. Destructuring Array hanya dapat dilakukan jika Array tersebut
+// tidak bernilai Null atau Undifined. Ketika melakukan destructuring pada Array yang
+// bernilai Null atau Undifined akan menyebabkan error.
+
+
